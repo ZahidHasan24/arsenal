b54b5da2b1c9a84c3dc714a4ca768880a5d95b3c
zahid hasan <zahid.hasan@asciisys.com>
2019-03-14 16:36:41 +0600

    arsenal team website made by react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './Resources/css/app.css';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import { firebase } from './firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+    return(
+        <BrowserRouter>
+            <Routes {...props}/>
+        </BrowserRouter>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+firebase.auth().onAuthStateChanged((user) => {
+    ReactDOM.render(<App user={user}/>, document.getElementById('root'));
+});
+
